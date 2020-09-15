f1bb2e107969f621ded4a9f7a63b2eb52d67f2ba
Justin Steinberg <jsteinberg@gmail.com>
2020-09-14 21:23:27 -0700

    setup updates

--- setup.py
+++ setup.py
@@ -8,11 +8,14 @@ setuptools.setup(
     version="0.0.1",
     author="Justin Steinberg",
     author_email="jsteinberg@gmail.com",
+    install_requires=["requests", "PyJWT", "requests_oauthlib"],
     description="Opinionated REST api client for Zoom Phone.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jsteinberg1/simple_zoomphone",
-    packages=setuptools.find_packages(),
+    packages=["simple_zoomphone"],
+    include_package_data=True,
+    platforms="any"
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU3 License",
